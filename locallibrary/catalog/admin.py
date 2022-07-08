557badfe2f39b5e4c6f8418006771292b94119a7
Daniel Rey <danielrey10252000@gmail.com>
2022-07-08 18:56:46 +0800

    Finished Part 4, Configurations(settings.py), add database and model also made some changes in admin ui

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
+
+# admin.site.register(Book)
+admin.site.register(Genre)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre',)
+
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstance(admin.ModelAdmin):
+    list_display = ('get_books', 'status', 'due_back', 'id',)
+    list_filter = ('status', 'due_back',)
+
+    fieldsets = (
+        (None, {'fields': ('book', 'imprint', 'id')}),
+        ('Avalability', {'fields': ('status', 'due_back')}),
+    )
+
+
+admin.site.register(Author, AuthorAdmin)
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'catalog.apps.CatalogConfig',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Singapore'
 
 USE_I18N = True
 
